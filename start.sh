a7ce241b19e9308de3d3ac23bc9d3bebffb9ae82
Gabriel Ulloa <86376043+Gabriel-Ulloa@users.noreply.github.com>
2023-11-20 20:56:40 -0800

    Changes

---
+++ start.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+myWHOAMI=$(whoami)
+if [ "$myWHOAMI" != "root" ]
+  then
+    sudo ./$0
+    exit
+fi
+
+week(){
+semana="week"
+
+dias=("Monday" "Tuesday" "Wednesday" "Thursday" "Friday" "Saturday")
+
+mkdir "/home/import/$semana"
+
+for dia in "${dias[@]}"; do
+    mkdir -p "/home/$semana/$dia"
+done
+
+}
+
+main(){
+  mkdir /home/import/sensor_catches
+  week
+  #cat /usr/local/bin/rcron | tee -a /etc/crontab
+}
+
+main
+
+mv "dionaea" "cowrie" "adbhoney" "variables" "decompressor" /usr/local/bin/
+
+apt install -y tcpdump wireshark sqlite
