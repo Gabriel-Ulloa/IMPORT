2b2337ffed8d6be9857e7e28f416e0a28c5e18a6
Gabriel Ulloa <86376043+Gabriel-Ulloa@users.noreply.github.com>
2023-11-21 21:43:52 -0800

    Update directories.sh

--- directories.sh
+++ directories.sh
@@ -4,17 +4,16 @@ mkdir /home/import/sensor_catches
 
 week(){
 
-semana="week"
+    semana="week"
 
-dias=("Monday" "Tuesday" "Wednesday" "Thursday" "Friday" "Saturday")
+    dias=("Monday" "Tuesday" "Wednesday" "Thursday" "Friday" "Saturday")
 
-mkdir "/home/import/$semana"
+    mkdir "/home/import/$semana"
 
-for dia in "${dias[@]}"; do
-    mkdir -p "/home/import/$semana/$dia"
-done
+    for dia in "${dias[@]}"; do
+        mkdir -p "/home/import/$semana/$dia"
+    done
 
 }
-
 
 week
