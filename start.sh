b188cc1dbfe8f90db6b47d854146b8e517982c65
Gabriel Ulloa <86376043+Gabriel-Ulloa@users.noreply.github.com>
2023-11-25 14:51:54 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 #
+# Ejecutar directories.sh y almacenar el código de salida
 bash directories.sh
-sudo bash installer.sh
+exit_status=$?
+
+# Verificar si directories.sh se ejecutó con éxito
+if [ $exit_status -eq 0 ]; then
+    echo "directories.sh executed successfully, proceeding with installer.sh."
+    sudo bash installer.sh
+else
+    echo "directories.sh failed to execute, exiting."
+    exit $exit_status
+fi
