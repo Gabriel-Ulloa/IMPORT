0019414290f5518bd543b6dbb16b3db6245ab775
Gabriel Ulloa <86376043+Gabriel-Ulloa@users.noreply.github.com>
2023-11-27 11:12:57 -0800

    Update VxAPI.sh

--- VxAPI.sh
+++ VxAPI.sh
@@ -1,10 +1,11 @@
 function hybrid_analysis(){
     file="/home/import/Deployment/CAPTURE/uniq_hashes.txt"
     VX_HASHES="/home/import/Deployment/CAPTURE/VX_HASHES"
+    vxapi="/home/import/Deployment/VxAPI/vxapi.py"
 
     counter=0
     while IFS= read -r hash; do
-        echo "consultando..." && /home/import/Deployment/VxAPI/vxapi.py search_hash $hash > $VX_HASHES/VX_$(echo $hash).yaml && sleep .5
+        echo "consultando..." && python $vxapi search_hash $hash > $VX_HASHES/VX_$(echo $hash).yaml && sleep .5
         counter=$((counter + 1))
         #Verificar si se han revisado doscientas líneas
         if ((counter % 200 == 0)); then
