7097184595b096602a655990a2fb1ac88a4b893e
Gabriel Ulloa <86376043+Gabriel-Ulloa@users.noreply.github.com>
2023-11-26 18:03:17 -0800

    Update directories.sh

--- directories.sh
+++ directories.sh
@@ -53,6 +53,21 @@ week(){
 
 }
 
+myAPI(){
+
+    myVT="https://github.com/VirusTotal/vt-cli/releases/download/0.14.0/Linux64.zip"
+    myVx="https://github.com/PayloadSecurity/VxAPI.git"
+    pip_py="https://bootstrap.pypa.io/get-pip.py"
+    
+
+    wget "$myVT" && unzip Linux64.zip 
+    git clone "$myVx"
+    curl $pip_py -o "get-pip.py"
+    python3 get-pip.py
+    pip3 install colorama
+
+}
+
 
 week
-#myAPI
+myAPI
